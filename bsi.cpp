865a4089a13a785cbb46b042f9b7d8c94fa6022d
Krzysztof Wachowiak <56433668+krzys2508@users.noreply.github.com>
2021-11-16 20:32:24 +0100

    making the user interface more user friendly

--- bsi.cpp
+++ bsi.cpp
@@ -51,32 +51,32 @@ double calculate_01_06(double mtbf, double time) {
 }
 int main (){
 
-    cout<< "Select task :"<<endl;
-    cout<<"1 = 01.03";
-    cout<<"2 = 01.04";
-    cout<<"3 = 01.05";
-    cout<<"4 = 01.06";
-    cout<<"5 = 03.06";
+     cout<< "Select task :"<<endl;
+    cout<<"1 = 01.03"<<endl;
+    cout<<"2 = 01.04"<<endl;
+    cout<<"3 = 01.05"<<endl;
+    cout<<"4 = 01.06"<<endl;
+    cout<<"5 = 03.06"<<endl;
     int selection; 
     cin>> selection;
     if (selection == 1 ){
-       cout<< calcuate_01_03(0.000001,2500);
+       cout<< calcuate_01_03(0.000001,2500)<<endl;
     }
     else if(selection==2){
-        cout<< calculate_01_04(98,5000);
+        cout<< calculate_01_04(98,5000)<<endl;
     }
     else if (selection==3){
         pair<double,double> result;
         result = calculate_01_05(25,30);
-        cout<<"Failure Rate : "<<result.first<<" Reliability : "<<result.second;
+        cout<<"Failure Rate : "<<result.first<<" Reliability : "<<result.second<<endl;
     }
     else if (selection==4){
-         cout<<calculate_01_06(1000,500);
+         cout<<calculate_01_06(1000,500)<<endl;
     }
     else if (selection==5){
 
 vector<int> population = {17,25,32,10,6,5,8,9,12,17,46,64,83,70,10,15,2,8,29,11};
-cout<<calculate36(population);
+cout<<calculate_03_06(population)<<endl;;
     }
 return 0;
 }
